6e2c60034f55ea0f37930d50f3adb4bf3f71047c
kapkap-madubye <105733109+kapkap-madubye@users.noreply.github.com>
2024-02-12 13:06:11 +0200

    Add files via upload

---
+++ TUT1_Question1/TUT1_Question1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class TUT1_Question1
+    {
+        static void Main(string[] args)
+        {
+            int[] numpack = new int[10];
+            for (int i = 0; i < numpack.Length; i++)
+            {
+                Console.WriteLine("Enter a number");
+                numpack[i] = int.Parse(Console.ReadLine());
+
+            }
+            int highnum = numpack[0];
+            int lowernum = numpack[0];
+
+            for (int i = 1; i < numpack.Length; i++)
+            {
+                if (numpack[i] >= highnum)
+                {
+                    highnum = numpack[i];
+                }
+                if (numpack[i] <= lowernum)
+                {
+                    lowernum = numpack[i];
+                }
+
+            }
+            for (int i = 0; i < numpack.Length; i++)
+            {
+                Console.WriteLine(numpack[i]);
+            }
+
+            Console.WriteLine("highest Number is :" + highnum);
+            Console.WriteLine("Lowest Number is : " + lowernum);
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
